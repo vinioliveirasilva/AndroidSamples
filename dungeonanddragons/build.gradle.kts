d127e8ec8dc4a68d82f257f8f071a4c4997b4f22
Vinicius Silva <ice_vsilva@uolinc.com>
2023-10-15 18:20:45 -0300

    Corrige build

--- dungeonanddragons/build.gradle.kts
+++ dungeonanddragons/build.gradle.kts
@@ -14,20 +14,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.6.0"))
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
